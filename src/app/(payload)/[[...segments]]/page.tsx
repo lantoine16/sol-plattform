9f71abbd0f34574acb30a4496de7a9c9ea105130
Lukas Antoine <lukas.antoine@gmx.net>
2025-11-28 21:52:54 +0100

    reset page.tsx to default value

--- src/app/(payload)/[[...segments]]/page.tsx
+++ src/app/(payload)/[[...segments]]/page.tsx
@@ -1,14 +1,9 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import type { Metadata } from 'next'
-import { headers as getHeaders } from 'next/headers.js'
-import { redirect } from 'next/navigation.js'
-import { getPayload } from 'payload'
-
 import config from '@payload-config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
 import { importMap } from '../importMap'
-import { USER_ROLE_ADMIN_VALUE } from '@/domain/constants/user-role.constants'
 
 type Args = {
   params: Promise<{
@@ -23,17 +18,6 @@ export const generateMetadata = ({ params, searchParams }: Args): Promise<Metada
   generatePageMetadata({ config, params, searchParams })
 
 const Page = async ({ params, searchParams }: Args) => {
-  // Check if user is admin before allowing access to admin panel
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  // If user is logged in but not an admin, redirect to login
-  if (user && user.role !== USER_ROLE_ADMIN_VALUE) {
-    redirect('/login')
-  }
-
   return RootPage({ config, params, searchParams, importMap })
 }
 
