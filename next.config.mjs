8dbe0c4168fb171b32d20062dfa9652cad56e171
Lukas Antoine <lukas.antoine@gmx.net>
2025-12-09 18:56:28 +0100

    addd github build-and-push.yml to build application with docker in github

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // Required for Docker deployment
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
