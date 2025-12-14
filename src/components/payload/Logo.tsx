7f0eff748524eb04ea11561576616355c4ffea34
Lukas Antoine <lukas.antoine@gmx.net>
2025-12-14 11:05:38 +0100

    Logo in breadcrum not clickable if you are on the dashboard already

--- src/components/payload/Logo.tsx
+++ src/components/payload/Logo.tsx
@@ -1,16 +1,20 @@
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function Logo() {
   const router = useRouter()
+  const pathname = usePathname()
+  const isHomePage = pathname === '/'
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault()
-    // Navigiere zu / ohne Search-Params
-    router.push('/')
+    if (!isHomePage) {
+      // Navigiere zu / ohne Search-Params
+      router.push('/')
+    }
   }
 
   return (
@@ -18,9 +22,10 @@ export default function Logo() {
       href="/"
       onClick={handleClick}
       style={{
-        cursor: 'pointer',
+        cursor: isHomePage ? 'default' : 'pointer',
         textDecoration: 'none',
         display: 'block',
+        pointerEvents: isHomePage ? 'none' : 'auto',
       }}
     >
       <Image src="/logo.png" alt="IGS Logo" width={100} height={100} />
